79648a1644094ba3428d015d0412edbdaa2834ff
Graham Ollis <perl@wdlabs.com>
2012-07-30 14:01:46 -0400

    use PodWeaver

--- lib/Task/BeLike/Plicease.pm
+++ lib/Task/BeLike/Plicease.pm
@@ -5,7 +5,18 @@ use warnings;
 use v5.10;
 
 # ABSTRACT: Modules Plicease Uses.
+
 # VERSION
+
+=head1 SYNOPSIS
+
+ cpan Task::BeLike::Plicease
+
+=head1 DESCRIPTION
+
+Modules that I like to have installed when I am doing my Perl thing.
+
+=cut
 
 use YAML ();
 use YAML::XS ();
@@ -45,28 +56,3 @@ use Test::Memory::Cycle ();
 
 1;
 
-__END__
-
-=head1 NAME
-
-Task::BeLike::Plicease - Modules Plicease Uses.
-
-=head1 SYNOPSIS
-
- cpan Task::BeLike::Plicease
-
-=head1 DESCRIPTION
-
-Modules that I like to have installed when I am doing my Perl thing.
-
-=head1 AUTHOR
-
-Graham Ollis <perl@wdlabs.com>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2012 by Ricardo Signes.
-
-This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
-
-=cut
