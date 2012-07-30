1809e53217bc8c7eee57f448f5a3d6a7c8532ae1
Graham Ollis <perl@wdlabs.com>
2012-07-30 13:49:22 -0400

    start out with some modules that I commonly use.

--- lib/Task/BeLike/Plicease.pm
+++ lib/Task/BeLike/Plicease.pm
@@ -4,6 +4,69 @@ use strict;
 use warnings;
 use v5.10;
 
+# ABSTRACT: Modules Plicease Uses.
 # VERSION
 
+use YAML ();
+use YAML::XS ();
+use JSON ();
+use JSON::XS ();
+use Pod::POM::Web ();
+use XML::Simple ();
+use XML::Writer ();
+use IPC::Run ();
+use Moose ();
+use AnyEvent ();
+use URI::Encode ();
+use Digest::MD5 ();
+use URI ();
+use Net::DNS ();
+use Test::File ();
+use Test::Dir ();
+use Net::DNS ();
+use Number::Bytes::Human ();
+use Mojolicious ();
+use App::Ack ();
+use Perl::Critic ();
+use Log::Log4perl ();
+use Template ();
+use App::cpanminus ();
+use File::chdir ();
+use DBI ();
+use DBD::SQLite ();
+use Test::EOL ();
+use Test::Strict ();
+use Test::Fixme ();
+use Test::NoTabs ();
+use Rose::DB ();
+use Rose::DB::Object ();
+use Test::Spelling ();
+use Test::Memory::Cycle ();
+
 1;
+
+__END__
+
+=head1 NAME
+
+Task::BeLike::Plicease - Modules Plicease Uses.
+
+=head1 SYNOPSIS
+
+ cpan Task::BeLike::Plicease
+
+=head1 DESCRIPTION
+
+Modules that I like to have installed when I am doing my Perl thing.
+
+=head1 AUTHOR
+
+Graham Ollis <perl@wdlabs.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Ricardo Signes.
+
+This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
+
+=cut
