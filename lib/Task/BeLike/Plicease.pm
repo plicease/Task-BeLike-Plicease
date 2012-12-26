682a68f1a9c304f616ddcb9aa1192a590a5db2ee
Graham Ollis <perl@wdlabs.com>
2012-12-26 14:22:30 -0500

    migrate to mostly having everything in the dist.ini

--- lib/Task/BeLike/Plicease.pm
+++ lib/Task/BeLike/Plicease.pm
@@ -1,33 +1,27 @@
 package Task::BeLike::Plicease;
 
 use strict;
-use warnings;
-use v5.10;
 
 # ABSTRACT: Modules Plicease Uses.
 # VERSION
 
 =head1 SYNOPSIS
 
- cpan Task::BeLike::Plicease
+ cpanm Task::BeLike::Plicease
 
 =head1 DESCRIPTION
 
 Modules that I like to have installed when I am doing my Perl thing.
 
 Some modules are not installed on MSWin32 or cygwin if they are known
-not to work on those platforms.  See the Makefile.PL that comes with
-this distribution for details.
+not to work on those platforms.  See the Makefile.PL or dist.ini that
+comes with this distribution for details.
 
 =over 4
 
 =item *
 
 AnyEvent
-
-=item *
-
-AnyEvent::Open3::Simple
 
 =item *
 
@@ -51,10 +45,6 @@ autodie
 
 =item *
 
-Clustericious
-
-=item *
-
 Clustericious::Client
 
 =item *
@@ -68,10 +58,6 @@ Clustericious::Log
 =item *
 
 Crypt::PasswdMD5
-
-=item *
-
-Data::Downloader
 
 =item *
 
@@ -211,10 +197,6 @@ Sereal::Decoder
 
 =item *
 
-Sereal::Encoder
-
-=item *
-
 Shell::Config::Generate
 
 =item *
@@ -243,15 +225,7 @@ Test::Dir
 
 =item *
 
-Test::Dir
-
-=item *
-
 Test::EOL
-
-=item *
-
-Test::File
 
 =item *
 
@@ -284,10 +258,6 @@ Test::Pod::Spelling::CommonMistakes
 =item *
 
 Test::Spelling
-
-=item *
-
-Test::Strict
 
 =item *
 
@@ -337,91 +307,4 @@ YAML::XS
 
 =cut
 
-use YAML ();
-use YAML::XS ();
-use JSON ();
-use JSON::XS ();
-use Pod::POM::Web ();
-use XML::Simple ();
-use XML::Writer ();
-use IPC::Run ();
-use Moose ();
-use AnyEvent ();
-use URI::Encode ();
-use Digest::MD5 ();
-use URI ();
-use Net::DNS ();
-use Test::File ();
-use Test::Dir ();
-use Number::Bytes::Human ();
-use Mojolicious ();
-use Mojolicious::Plugin::TtRenderer ();
-use Mojolicious::Plugin::SslAuth ();
-use App::Ack ();
-use Perl::Critic ();
-use Log::Log4perl ();
-use Template ();
-use App::cpanminus ();
-use File::chdir ();
-use DBI ();
-use DBD::SQLite ();
-use Test::EOL ();
-use Test::Fixme ();
-use Test::NoTabs ();
-use Rose::DB ();
-use Rose::DB::Object ();
-use Test::Spelling ();
-use Test::Memory::Cycle ();
-use Test::Pod ();
-use Test::Pod::Coverage ();
-use Test::Pod::Spelling::CommonMistakes ();
-use Test::File ();
-use Test::Dir ();
-use Module::Which ();
-use Lingua::Alphabet::Phonetic::LAPD ();
-use Mojolicious::Plugin::Ident ();
-use Devel::StackTrace ();
-use Clustericious::Client ();
-use Clustericious::Config ();
-use Clustericious::Log ();
-use String::Template ();
-use autodie ();
-use DBD::PgPP ();
-use DBD::mysqlPP ();
-use Test::Differences ();
-use App::RegexFileUtils ();
-use Shell::Config::Generate ();
-use Shell::Guess ();
-use WebService::TwitterBootstrap::Download::Custom ();
-use Role::Tiny ();
-use Scalar::Does ();
-use Crypt::PasswdMD5 ();
-use File::Touch ();
-use Authen::Simple ();
-use Mojolicious::Plugin::RenderFile ();
-use Mojo::JSON::Any ();
-use HTTP::Tiny ();
-use Try::Tiny ();
-use Template::Tiny ();
-
-# core, but in separate rpms for Fedora
-use Pod::Perldoc ();
-use Module::CoreList ();
-
-# http://www.modernperlbooks.com/mt/2012/10/i-stopped-parsing-xml-thanks-to-xmlrabbit.html
-use XML::Rabbit ();
-use File::IgnoreReadonly ();
-# http://blog.booking.com/sereal-a-binary-data-serialization-format.html
-#use Sereal::Encoder ();
-use Sereal::Decoder ();
-
-# TODO: Rose::Planter
-# and   Module::Build::Database
-# would be nice but depend on DBD::Pg
-
-# look at these later:
-use TinyMake ();
-use Validate::Tiny ();
-
 1;
-
