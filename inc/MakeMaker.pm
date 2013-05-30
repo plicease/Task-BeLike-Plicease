bb9824d5196f85775e347bf92f4d98d2a88b3efe
Graham Ollis <perl@wdlabs.com>
2013-05-30 14:21:32 -0400

    prep for release

---
+++ inc/MakeMaker.pm
@@ -0,0 +1,29 @@
+package inc::MakeMaker;
+
+use Moose;
+use namespace::autoclean;
+use v5.10;
+
+with 'Dist::Zilla::Role::InstallTool';
+
+sub setup_installer
+{
+  my($self) = @_;
+  
+  my($makefile) = grep { $_->name eq 'Makefile.PL' } @{ $self->zilla->files };
+  
+  my $content = $makefile->content;
+  
+  if($content =~ s{\t}{ }gm)
+  {
+    $makefile->content($content);
+    $self->zilla->log("Modified Makefile.PL with extra checks");
+  }
+  else
+  {
+    $self->zilla->log_fatal("unable to update Makefile.PL");
+  }
+}
+
+1;
+
